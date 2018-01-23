5712cc8e3356a9cafd6de6c03a7fb438b4088b46
Ilya Osipov <070809010@mail.ru>
2018-01-23 20:39:28 +0300

    Добавлен app для auth системы с моделью User

---
+++ registration/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+ROLE_CHOICES = (
+    ('Admin', 'Admin'),
+    ('Adept', 'Adept'),
+    ('User', 'User'),
+)
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+    first_name = models.CharField(
+        max_length=255,
+    )
+    last_name = models.CharField(
+        max_length=255,
+    )
+    vk_id = models.CharField(
+        max_length=20,
+    )
+    role = models.CharField(
+        max_length=5,
+        choices=ROLE_CHOICES,
+        default='User',
+    )
+    about = models.TextField()
+    picture = models.ImageField()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username
