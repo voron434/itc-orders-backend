558f63878feb5bc34d090c7ea98df47925f2b0d2
Ilya Osipov <070809010@mail.ru>
2018-01-24 23:16:22 +0300

    Добавлена модель заказа

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from registration.models import User
+
+ORDER_STATUSES_CHOICES = (
+    ('New', 'New'),
+    ('Rejected', 'Rejected'),
+    ('Approved', 'Approved'),
+    ('Research', 'Research'),
+    ('Archieved', 'Archieved'),
+)
+
+
+class Order(models.Model):
+    title = models.CharField(
+        max_length=40,
+        unique=True,
+    )
+    description = models.TextField()
+    orderer = models.ForeignKey(User)
+    status = models.CharField(
+        max_length=10,
+        choices=ORDER_STATUSES_CHOICES,
+        default='New',
+    )
+
+    def __str__(self):
+        return self.title
