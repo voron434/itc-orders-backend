c95763de43af7ed1acbb01a1c7ee233c4d5ca70d
Ilya Osipov <070809010@mail.ru>
2018-01-25 00:52:49 +0300

    Добавлена проверка входа

--- stock/views.py
+++ stock/views.py
@@ -1,10 +1,12 @@
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 
 from . import models
 
 
 @ensure_csrf_cookie
+@login_required
 def create_order(request):
     if request.POST:
         title = request.POST.get('title')
