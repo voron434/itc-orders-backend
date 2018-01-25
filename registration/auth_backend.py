64f9872c37a7ea4a8c460b3be1ca86102be79330
Ilya Osipov <070809010@mail.ru>
2018-01-25 04:12:51 +0300

    Добавил вход по паролю

--- registration/auth_backend.py
+++ registration/auth_backend.py
@@ -4,7 +4,6 @@ from .models import User
 
 class PasswordlessAuthBackend(ModelBackend):
     """Log in to Django without providing a password.
-
     """
     def authenticate(self, vk_id=None):
         try:
@@ -14,6 +13,29 @@ class PasswordlessAuthBackend(ModelBackend):
 
     def get_user(self, user_id):
         try:
-            return User.objects.get(pk=user_id)
+            return User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return None
+
+
+class PasswordAuthBackend(ModelBackend):
+    """Log in to Django without providing a password.
+    """
+    def authenticate(self, username=None, password=None):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a nonexistent user (#20760).
+            User().set_password(password)
+            return None
+        else:
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
+        return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(id=user_id)
         except User.DoesNotExist:
             return None
