fe6fd7d6f36ee6825611bcb20b313c4c670f1a50
Ilya Osipov <070809010@mail.ru>
2018-02-07 07:39:53 +0300

    Добавлены логи

--- registration/views.py
+++ registration/views.py
@@ -4,11 +4,17 @@ from django.http import JsonResponse
 import vk
 from vk.exceptions import VkAPIError
 
+import logging
+
 from .models import User
 from .serializers import UserSerializer
 
 
+logger = logging.getLogger('admin')
+
+
 def vk_auth(request):
+    logger.debug('Starting handling auth request...')
     vk_token = request.GET.get('access_token')
     if vk_token:
         session = vk.Session()
@@ -16,19 +22,25 @@ def vk_auth(request):
         try:
             user_info = api.users.get(access_token=vk_token, fields=['photo_200', ])[0]
         except VkAPIError as e:
+            logger.error(e.message)
+            logger.debug('Handling done with an error')
             return JsonResponse({'error': {'code': e.code, 'message': e.message}})
+        logger.debug('Successfully got vk response')
         user_id = user_info['id']
         same_user = User.objects.filter(vk_id=user_id)
         if same_user.exists():
+            logger.debug('Such user exists...')
             user = authenticate(vk_id=user_id)
             login(request, user)
             json_user = UserSerializer(user)
+            logger.debug('Handling done with success')
             return JsonResponse({
                 'success': True,
                 'user': json_user.data
             })
 
         else:
+            logger.debug('Creating record for new user...')
             new_user = User(
                 vk_id=user_id,
                 first_name=user_info['first_name'],
@@ -40,9 +52,11 @@ def vk_auth(request):
             user = authenticate(vk_id=user_id)
             login(request, user)
             json_user = UserSerializer(user)
+            logger.debug('Handling done with success')
             return JsonResponse({
                 'success': True,
                 'user': json_user.data
             })
-
+    logger.error('Some unexpected error')
+    logger.debug('Handling done with an error')
     return JsonResponse({'error': True})
